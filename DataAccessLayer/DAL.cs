e9de472a17d8456842a78ff63ae8c7b39681970f
Serdar Damla <serdar.damla12@gmail.com>
2022-06-10 23:06:28 +0300

    Add project files.

---
+++ DataAccessLayer/DAL.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+
+namespace DataAccessLayer
+{
+    public class DBFacade
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+        private string connectionString = string.Empty;
+
+        public DBFacade(string ConnectionString)
+        {
+            this.connectionString = ConnectionString;
+            connection = new SqlConnection(connectionString);
+        }
+        private void Connect()
+        {
+            if (connection != null)
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    try
+                    {
+                        connection.Open();
+                    }
+                    catch (SqlException exception)
+                    {
+                        throw exception;
+                    }
+                }
+            }
+        }
+        private void Disconnect()
+        {
+            if (connection != null)
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+        public DataTable GetTable(string query, bool isProcedure, params SqlParameter[] sqlParameters)
+        {
+            Connect();
+            command = connection.CreateCommand();
+            command.CommandText = query;
+            if (isProcedure)
+            {
+                command.CommandType = CommandType.StoredProcedure;
+            }
+            else
+            {
+                command.CommandType = CommandType.Text;
+            }
+            foreach (var parameters in sqlParameters)
+            {
+                command.Parameters.Add(parameters);
+            }
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            Disconnect();
+            return table;
+        }
+        public bool Execute(string query, bool isProcedure, params SqlParameter[] sqlParameters)
+        {
+            Connect();
+            command = connection.CreateCommand();
+            command.CommandText = query;
+
+            if (isProcedure)
+            {
+                command.CommandType = CommandType.StoredProcedure;
+            }
+            else
+            {
+                command.CommandType = CommandType.Text;
+            }
+            foreach (var parameters in sqlParameters)
+            {
+                command.Parameters.Add(parameters);
+            }
+            int tmp = command.ExecuteNonQuery();
+            Disconnect();
+            return (tmp != 0);
+        }
+        public long GetScalar(string query, bool isProcedure, params SqlParameter[] sqlParameters)
+        {
+            Connect();
+            command = connection.CreateCommand();
+            command.CommandText = query;
+
+            if (isProcedure)
+            {
+                command.CommandType = CommandType.StoredProcedure;
+            }
+            else
+            {
+                command.CommandType = CommandType.Text;
+            }
+            
+            foreach (var parameters in sqlParameters)
+            {
+                command.Parameters.Add(parameters);
+            }
+            long tmp = (long)command.ExecuteScalar();
+            Disconnect();
+            return tmp;
+        }
+    }
+}
